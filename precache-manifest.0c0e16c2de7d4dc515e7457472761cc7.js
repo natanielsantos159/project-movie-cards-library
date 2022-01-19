5eae270b1ad57125e137a4974d8f1d6a4dee5008
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-18 21:23:28 -0300

    Updates

---
+++ precache-manifest.0c0e16c2de7d4dc515e7457472761cc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c8a35b18ab10ca6add678c90297105",
+    "url": "/project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "6f069de463cba0e9df00",
+    "url": "/project-movie-cards-library/static/css/main.0f650810.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/project-movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "6f069de463cba0e9df00",
+    "url": "/project-movie-cards-library/static/js/main.954fd87e.chunk.js"
+  },
+  {
+    "revision": "b5cd559347f125a80e22",
+    "url": "/project-movie-cards-library/static/js/runtime-main.136c6182.js"
+  }
+]);
